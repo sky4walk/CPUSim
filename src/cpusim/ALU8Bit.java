e1d3054accdf9ee71efb808ffa4268eb8030d2fb
Andre Betz <git@andrebetz.de>
2019-09-11 22:25:53 +0200

    alu implemented

--- src/cpusim/ALU8Bit.java
+++ src/cpusim/ALU8Bit.java
@@ -24,6 +24,13 @@ public class ALU8Bit {
     private mux8Bit2x mux1;
     private mux8Bit2x mux2;
     private mux8Bit2x mux3;
+    private DataLine8Bit lineAddMux1 = new DataLine8Bit();
+    private DataLine8Bit lineSubMux1 = new DataLine8Bit();
+    private DataLine8Bit lineAndMux2 = new DataLine8Bit();
+    private DataLine8Bit lineOrMux2 = new DataLine8Bit();
+    private DataLine8Bit lineMux1Mux3 = new DataLine8Bit();
+    private DataLine8Bit lineMux2Mux3 = new DataLine8Bit();
+    private DataLine8Bit lineNotSub = new DataLine8Bit();
     public ALU8Bit(
             DataLine8Bit inALine,
             DataLine8Bit inBLine,
@@ -32,9 +39,24 @@ public class ALU8Bit {
         inA = inALine;
         inB = inBLine;
         out = outLine;
-        opSelect = opSelectLine;
+        opSelect = opSelectLine;        
+        not1 = new Not8Bit(inB, lineNotSub);
+        add1 = new Add8Bit(inA, inB, lineAddMux1, uZero, new DataLine1Bit());
+        sub1 = new Add8Bit(inA, lineNotSub, lineSubMux1, uOne, new DataLine1Bit());
+        and1 = new And8Bit(inA, inB, lineAndMux2);
+        or1  = new Or8Bit(inA, inB, lineOrMux2);
+        mux1 = new mux8Bit2x(lineAddMux1, lineSubMux1, lineMux1Mux3, opSelect.getDataLine(0));
+        mux2 = new mux8Bit2x(lineAndMux2, lineOrMux2, lineMux2Mux3, opSelect.getDataLine(0));
+        mux3 = new mux8Bit2x(lineMux1Mux3, lineMux2Mux3, out, opSelect.getDataLine(1));
     }
     public void calc() {
-        
+        not1.calc();
+        add1.calc();
+        sub1.calc();
+        and1.calc();
+        or1.calc();
+        mux1.calc();
+        mux2.calc();
+        mux3.calc();
     }
 }
--- src/cpusim/DataLine8Bit.java
+++ src/cpusim/DataLine8Bit.java
@@ -10,35 +10,44 @@ package cpusim;
  * @author andre
  */
 public class DataLine8Bit {
-    private boolean pins[] = new boolean[8];
+    private DataLine1Bit lines[] = new DataLine1Bit[8];
     public DataLine8Bit() {
+        for ( int i = 0; i < lines.length; i++ ) {
+            lines[i] = new DataLine1Bit();
+        }
         reset();
     }
     public void reset() {
-        for ( int i = 0; i < pins.length; i++ ) {
-            setPin(i,false);
+        for ( int i = 0; i < lines.length; i++ ) {
+            lines[i].reset();
         }        
     }
     public void setPin(int nr,boolean val) {
-        if ( nr < pins.length ) {
-            pins[nr] = val;
+        if ( nr < lines.length ) {
+            lines[nr].setPin(0, val);
         }
     }
     public boolean getPin(int nr) {
-        if ( nr < pins.length ) {
-            return pins[nr];
+        if ( nr < lines.length ) {
+            return lines[nr].getPin(0);
         }
         return false;
     }
-    public void setPins(DataLine8Bit bits) {
-        for ( int i = 0; i < pins.length; i++ ) {
+    public DataLine1Bit getDataLine(int nr) {
+        if ( nr < lines.length ) {
+            return lines[nr];
+        }
+        return null;        
+    }
+    public void setPins(DataLine1Bit bits) {
+        for ( int i = 0; i < lines.length; i++ ) {
             setPin(i,bits.getPin(i));
         }
     }
     public String getBitsString() {
         String bits = "";
-        for ( int i = 0; i < pins.length; i++ ) {
-            if ( pins[pins.length - 1 - i] ) {
+        for ( int i = 0; i < lines.length; i++ ) {
+            if ( getPin(lines.length - 1 - i) ) {
                 bits += "1";
             } else {
                 bits += "0";
@@ -49,8 +58,8 @@ public class DataLine8Bit {
     public int getBitsInt() {
         int bits = 0;
         int cnt = 1;
-        for ( int i = 0; i < pins.length; i++ ) {
-            if ( true == pins[i] ) {
+        for ( int i = 0; i < lines.length; i++ ) {
+            if ( true == getPin(i) ) {
                 bits += cnt;
             }
             cnt *= 2;
@@ -59,10 +68,10 @@ public class DataLine8Bit {
     }
     public void setBitsString(String val) {
         reset();
-        if ( val.length() == pins.length ) {
-            for ( int i = 0; i < pins.length; i++ ) {
+        if ( val.length() == lines.length ) {
+            for ( int i = 0; i < lines.length; i++ ) {
                 if ( '1' == val.charAt(i) ) {
-                    setPin(pins.length - 1 - i, true);
+                    setPin(lines.length - 1 - i, true);
                 } 
             }
         }
@@ -72,7 +81,7 @@ public class DataLine8Bit {
         int tmp = val;
         int exp = 1;
         int i = 0;
-        while ( 0 != tmp && i < pins.length ) {
+        while ( 0 != tmp && i < lines.length ) {
             if ( 1 == tmp % 2 ) {
                 setPin(i, true);
             } 
@@ -81,5 +90,5 @@ public class DataLine8Bit {
             exp *= 2;
         }
         if ( exp <= val ) reset();
-    }
+    } 
 }
