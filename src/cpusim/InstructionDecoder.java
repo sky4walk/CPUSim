a281d2f23f7f6ab3b439068634b264313e92c4e3
Andre Betz <git@andrebetz.de>
2019-09-20 09:59:34 +0200

    instruction decoder

---
+++ src/cpusim/InstructionDecoder.java
@@ -0,0 +1,13 @@
+/*
+ * https://itzwieseltal.wordpress.com/2018/01/23/do-it-yourself-cpu-4-prozessor/
+ * github@AndreBetz.de
+ */
+package cpusim;
+
+/**
+ *
+ * @author betzan8u
+ */
+public class InstructionDecoder {
+    
+}
